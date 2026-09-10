365f78aa5c69f40bc42e2f79d89031e03610458e
Ishika Dutta <ishikadutta826@gmail.com>
2026-09-10 13:30:09 +0530

    feat: connect backend to MongoDB Atlas using Mongoose

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,11 @@
 import express from "express";
+import dotenv from "dotenv";
+import { connectDB } from "./lib/db.js";
+
+dotenv.config();
 
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 app.get("/", (req, res) => {
   res.send("Hello from the backend!");
@@ -9,4 +13,5 @@ app.get("/", (req, res) => {
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
+  connectDB();
 });
