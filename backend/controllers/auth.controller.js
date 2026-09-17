c36a352920445a93aea7116e74246583ac23a787
Ishika Dutta <ishikadutta826@gmail.com>
2026-09-18 03:42:06 +0530

    feat: add profile picture upload with Cloudinary

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,7 +1,7 @@
 import bcrypt from "bcryptjs";
 import User from "../models/user.model.js";
 import { generateToken } from "../lib/utils.js";
-
+import cloudinary from "../lib/cloudinary.js";
 export const signup = async (req, res) => {
   const { fullName, email, password } = req.body;
   try {
@@ -107,6 +107,39 @@ export const checkAuth = (req, res) => {
     return res.status(200).json(req.user);
   } catch (error) {
     console.log("Error in checkAuth controller:", error.message);
+
+    return res.status(500).json({
+      message: "Internal Server Error",
+    });
+  }
+};
+
+export const updateProfile = async (req, res) => {
+  try {
+    const { profilePic } = req.body;
+    const userId = req.user._id;
+
+    if (!profilePic) {
+      return res.status(400).json({
+        message: "Profile picture is required",
+      });
+    }
+
+    const uploadResponse = await cloudinary.uploader.upload(profilePic);
+
+    const updatedUser = await User.findByIdAndUpdate(
+      userId,
+      {
+        profilePic: uploadResponse.secure_url,
+      },
+      {
+        new: true,
+      }
+    ).select("-password");
+
+    return res.status(200).json(updatedUser);
+  } catch (error) {
+    console.log("Error in updateProfile controller:", error.message);
 
     return res.status(500).json({
       message: "Internal Server Error",
