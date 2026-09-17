eb2bf404aaaf58eb9776e2e526655ec6762cd335
Ishika Dutta <ishikadutta826@gmail.com>
2026-09-18 02:06:02 +0530

    feat: add signup login and logout authentication

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,18 @@
 import express from "express";
 import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
 import { connectDB } from "./lib/db.js";
+import authRoutes from "./routes/auth.route.js";
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+
+app.use(express.json());
+app.use(cookieParser());
+
+app.use("/api/auth", authRoutes);
 
 app.get("/", (req, res) => {
   res.send("Hello from the backend!");
