904715335cd8549955eac3d63654e9b7fb466af7
Ishika Dutta <ishikadutta826@gmail.com>
2026-09-18 12:21:40 +0530

    feat: add real-time messaging with Socket.IO

--- backend/index.js
+++ backend/index.js
@@ -4,10 +4,10 @@ import cookieParser from "cookie-parser";
 import { connectDB } from "./lib/db.js";
 import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
-
+import { app, server } from "./lib/socket.js";
 dotenv.config();
 
-const app = express();
+
 const PORT = process.env.PORT || 5000;
 
 app.use(express.json({ limit: "10mb" }));
@@ -19,7 +19,7 @@ app.get("/", (req, res) => {
   res.send("Hello from the backend!");
 });
 
-app.listen(PORT, () => {
+server.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
   connectDB();
 });
